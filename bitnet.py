7a69de32b97b4dbde5abc7a13dfff14b2f4de964
aloobun <145441993+aloobun@users.noreply.github.com>
2024-04-15 20:21:36 +0530

    absmax_quantize

--- bitnet.py
+++ bitnet.py
@@ -22,12 +22,27 @@ class BitLinear(nn.Linear):
     def __init__(self, in_features, out_features, bias=False):
         super(BitLinear, self).__init__(in_features, out_features, bias, rms_norm_eps=1e-8)
         self.layernorm = BitRMSNorm(hidden_size=in_features, eps=rms_norm_eps)
+
+    def absmax_quantize(self, x):
+        epsilon = 1e-6
+        if self.flg_before_linear:
+            gamma = torch.abs(x).max().clamp(min=epsilon)
+            x_scaled = x * self.Qb / gamma
+            x_q = torch.round(x_scaled).clamp(-self.Qb, self.Qb - 1)
+        else:
+            eta = x.min()
+            gamma = torch.abs(x - eta).max().clamp(min=epsilon)
+            x_scaled = (x - eta) * self.Qb / gamma
+            x_q = torch.round(x_scaled).clamp(0, self.Qb - 1)
+        
+        return x_q, gamma
         
     def forward(self, x):
         # layernorm (input: x, output: x_norm)
         x_norm = self.layernorm(x)
 
         # absmax quatization (input: x_norm, output: x_q,gamma)
+        x_q, gamma = self.absmax_quantize(x_norm)
 
         # 1 bit weights (input: -, output: w_q, beta)
 
